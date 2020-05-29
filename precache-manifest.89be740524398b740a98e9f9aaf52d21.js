1277accf2e9af029f4f9c2c9ebb89fa370954acf
Mowmowj <274334790@qq.com>
2020-05-29 20:38:23 +0800

    publish nexo ui page

---
+++ precache-manifest.89be740524398b740a98e9f9aaf52d21.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c341edb25ddaa76d70698128fad65335",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.612165914a4590f41e14.bundle.js"
+  },
+  {
+    "url": "runtime~main.612165914a4590f41e14.bundle.js"
+  },
+  {
+    "revision": "2d244c7ec18134e74f45",
+    "url": "static/css/main.0974272a.chunk.css"
+  },
+  {
+    "revision": "e205ee87f7f6d4aabb69",
+    "url": "static/css/vendors~main.f8f248a5.chunk.css"
+  },
+  {
+    "url": "vendors~main.612165914a4590f41e14.bundle.js"
+  },
+  {
+    "url": "vendors~main.612165914a4590f41e14.bundle.js.LICENSE.txt"
+  }
+]);
